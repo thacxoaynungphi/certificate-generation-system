9c0767150fb4b54798661b249c786cdd09e1a1f9
nhchung.it <nhchung.it@gmail.com>
2011-03-09 13:14:18 +0000

    Test thu hieu qua khong

---
+++ Certificate Generation System/src/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nhchung
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("huu chung");
+    }
+}
