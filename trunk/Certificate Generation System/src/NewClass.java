d23807aa55fa9e0c4d449dd24cda0c046af7cfa6
daohuubien@gmail.com <daohuubien@gmail.com@d9e485e5-d9bd-12cd-a623-42b7acfffde7>
2011-03-10 09:06:15 +0000

    chỉnh sửa NewClass

--- trunk/Certificate Generation System/src/NewClass.java
+++ trunk/Certificate Generation System/src/NewClass.java
@@ -10,5 +10,6 @@
 public class NewClass {
     public static void main(String[] args) {
         System.out.println("Hai anh dep trai qua");
+        System.out.println("Anh biên có mặt");
     }
 }
