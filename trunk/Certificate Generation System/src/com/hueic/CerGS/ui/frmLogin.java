15708d8bc9251bf748a90cb0d6a04e8625d0c9e0
svhaiduong90@gmail.com <svhaiduong90@gmail.com@d9e485e5-d9bd-12cd-a623-42b7acfffde7>
2011-03-13 22:01:10 +0000

    Edit lang

--- trunk/Certificate Generation System/src/com/hueic/CerGS/ui/frmLogin.java
+++ trunk/Certificate Generation System/src/com/hueic/CerGS/ui/frmLogin.java
@@ -46,11 +46,14 @@ public class frmLogin extends javax.swing.JDialog {
 
     public void bindingLang() {
         btnLogin.setText(langProperties.getValueLanguage("LOGIN"));
-        btnCancel.setText(langProperties.getValueLanguage("CANCEL"));
+        btnExit.setText(langProperties.getValueLanguage("EXIT"));
+        btnReset.setText(langProperties.getValueLanguage("RESET"));
         lblUsername.setText(langProperties.getValueLanguage("USERNAME"));
         lblPassword.setText(langProperties.getValueLanguage("PASSWORD"));
         lblPermission.setText(langProperties.getValueLanguage("PERMISSION"));
         chbRepass.setText(langProperties.getValueLanguage("REMEMBER_PASSWORD"));
+         panelInput.setBorder(javax.swing.BorderFactory.createTitledBorder(null, langProperties.getValueLanguage("LOGIN_SYSTEM"), javax.swing.border.TitledBorder.LEFT, javax.swing.border.TitledBorder.DEFAULT_POSITION));
+        
     }
 
     public void bindingData() {
@@ -75,84 +78,106 @@ public class frmLogin extends javax.swing.JDialog {
         java.awt.GridBagConstraints gridBagConstraints;
 
         jPasswordField1 = new javax.swing.JPasswordField();
-        txtPassword = new javax.swing.JPasswordField();
-        lblUsername = new javax.swing.JLabel();
-        lblPassword = new javax.swing.JLabel();
-        lblPermission = new javax.swing.JLabel();
-        txtUsername = new javax.swing.JTextField();
-        cbxPermission = new javax.swing.JComboBox();
-        chbRepass = new javax.swing.JCheckBox();
         jPanel1 = new javax.swing.JPanel();
+        panelInput = new javax.swing.JPanel();
+        lblPermission = new javax.swing.JLabel();
+        lblUsername = new javax.swing.JLabel();
+        jSeparator = new javax.swing.JSeparator();
+        txtPassword = new javax.swing.JPasswordField();
+        lblPassword = new javax.swing.JLabel();
+        cbxPermission = new javax.swing.JComboBox();
+        txtUsername = new javax.swing.JTextField();
+        chbRepass = new javax.swing.JCheckBox();
+        jPanel3 = new javax.swing.JPanel();
         btnLogin = new javax.swing.JButton();
-        btnCancel = new javax.swing.JButton();
-        jPanel2 = new javax.swing.JPanel();
-        jSeparator1 = new javax.swing.JSeparator();
+        btnReset = new javax.swing.JButton();
+        btnExit = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         jPasswordField1.setText("jPasswordField1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Login System");
         setMinimumSize(new java.awt.Dimension(400, 300));
-        setResizable(false);
-        getContentPane().setLayout(new java.awt.GridBagLayout());
 
-        txtPassword.setPreferredSize(new java.awt.Dimension(200, 20));
+        jPanel1.setLayout(new java.awt.GridBagLayout());
+
+        panelInput.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Login System", javax.swing.border.TitledBorder.LEFT, javax.swing.border.TitledBorder.DEFAULT_POSITION));
+        panelInput.setLayout(new java.awt.GridBagLayout());
+
+        lblPermission.setText("Permission:");
         gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 2;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHEAST;
-        gridBagConstraints.insets = new java.awt.Insets(0, 5, 5, 20);
-        getContentPane().add(txtPassword, gridBagConstraints);
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.insets = new java.awt.Insets(5, 20, 5, 0);
+        panelInput.add(lblPermission, gridBagConstraints);
 
         lblUsername.setText("Username:");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 1;
+        gridBagConstraints.gridy = 0;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
-        gridBagConstraints.insets = new java.awt.Insets(0, 20, 5, 5);
-        getContentPane().add(lblUsername, gridBagConstraints);
+        gridBagConstraints.insets = new java.awt.Insets(5, 20, 5, 0);
+        panelInput.add(lblUsername, gridBagConstraints);
+
+        jSeparator.setPreferredSize(new java.awt.Dimension(400, 10));
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 7;
+        gridBagConstraints.gridwidth = 6;
+        gridBagConstraints.ipadx = 401;
+        gridBagConstraints.ipady = 9;
+        gridBagConstraints.insets = new java.awt.Insets(5, 0, 0, 0);
+        panelInput.add(jSeparator, gridBagConstraints);
+
+        txtPassword.setPreferredSize(new java.awt.Dimension(200, 20));
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.ipadx = 200;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.insets = new java.awt.Insets(5, 40, 5, 0);
+        panelInput.add(txtPassword, gridBagConstraints);
 
         lblPassword.setText("Password:");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 2;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
-        gridBagConstraints.insets = new java.awt.Insets(0, 20, 5, 5);
-        getContentPane().add(lblPassword, gridBagConstraints);
-
-        lblPermission.setText("Permission:");
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 3;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
-        gridBagConstraints.insets = new java.awt.Insets(0, 20, 5, 5);
-        getContentPane().add(lblPermission, gridBagConstraints);
-
-        txtUsername.setPreferredSize(new java.awt.Dimension(200, 20));
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHEAST;
-        gridBagConstraints.insets = new java.awt.Insets(0, 5, 5, 20);
-        getContentPane().add(txtUsername, gridBagConstraints);
+        gridBagConstraints.insets = new java.awt.Insets(5, 20, 5, 0);
+        panelInput.add(lblPassword, gridBagConstraints);
 
         cbxPermission.setPreferredSize(new java.awt.Dimension(200, 20));
         gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 3;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHEAST;
-        gridBagConstraints.insets = new java.awt.Insets(0, 5, 5, 20);
-        getContentPane().add(cbxPermission, gridBagConstraints);
+        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.ipadx = 100;
+        gridBagConstraints.ipady = 2;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.insets = new java.awt.Insets(5, 40, 5, 4);
+        panelInput.add(cbxPermission, gridBagConstraints);
+
+        txtUsername.setPreferredSize(new java.awt.Dimension(200, 20));
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.ipadx = 200;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.insets = new java.awt.Insets(5, 40, 5, 0);
+        panelInput.add(txtUsername, gridBagConstraints);
 
         chbRepass.setText("Remember password");
         gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 4;
-        gridBagConstraints.ipadx = 76;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
-        gridBagConstraints.insets = new java.awt.Insets(2, 5, 5, 20);
-        getContentPane().add(chbRepass, gridBagConstraints);
+        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridy = 6;
+        gridBagConstraints.ipadx = 77;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.insets = new java.awt.Insets(5, 40, 5, 0);
+        panelInput.add(chbRepass, gridBagConstraints);
 
-        jPanel1.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.CENTER, 10, 5));
+        jPanel3.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.CENTER, 10, 5));
 
         btnLogin.setText("Login");
         btnLogin.addActionListener(new java.awt.event.ActionListener() {
@@ -160,59 +185,56 @@ public class frmLogin extends javax.swing.JDialog {
                 btnLoginActionPerformed(evt);
             }
         });
-        jPanel1.add(btnLogin);
+        jPanel3.add(btnLogin);
 
-        btnCancel.setText("Cancel");
-        btnCancel.setPreferredSize(new java.awt.Dimension(85, 23));
-        btnCancel.addActionListener(new java.awt.event.ActionListener() {
+        btnReset.setText("Reset");
+        btnReset.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnCancelActionPerformed(evt);
+                btnResetActionPerformed(evt);
             }
         });
-        jPanel1.add(btnCancel);
+        jPanel3.add(btnReset);
+
+        btnExit.setText("Exit");
+        btnExit.setPreferredSize(new java.awt.Dimension(85, 23));
+        btnExit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnExitActionPerformed(evt);
+            }
+        });
+        jPanel3.add(btnExit);
 
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 6;
-        gridBagConstraints.gridwidth = 2;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.SOUTH;
-        gridBagConstraints.insets = new java.awt.Insets(0, 10, 5, 10);
-        getContentPane().add(jPanel1, gridBagConstraints);
+        gridBagConstraints.gridy = 8;
+        gridBagConstraints.gridwidth = 5;
+        gridBagConstraints.ipadx = 34;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.insets = new java.awt.Insets(0, 40, 10, 0);
+        panelInput.add(jPanel3, gridBagConstraints);
 
-        jPanel2.setPreferredSize(new java.awt.Dimension(400, 100));
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.insets = new java.awt.Insets(5, 0, 5, 0);
+        jPanel1.add(panelInput, gridBagConstraints);
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/hueic/CerGS/images/welcome.jpg"))); // NOI18N
+        jPanel1.add(jLabel1, new java.awt.GridBagConstraints());
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 100, Short.MAX_VALUE)
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 306, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
-
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.gridwidth = 2;
-        gridBagConstraints.insets = new java.awt.Insets(0, 0, 10, 0);
-        getContentPane().add(jPanel2, gridBagConstraints);
-
-        jSeparator1.setPreferredSize(new java.awt.Dimension(400, 10));
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 5;
-        gridBagConstraints.gridwidth = 2;
-        getContentPane().add(jSeparator1, gridBagConstraints);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void btnCancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCancelActionPerformed
-        // TODO add your handling code here:
-        this.dispose();
-    }//GEN-LAST:event_btnCancelActionPerformed
 
     private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoginActionPerformed
         // TODO add your handling code here:
@@ -232,6 +254,18 @@ public class frmLogin extends javax.swing.JDialog {
         }
     }//GEN-LAST:event_btnLoginActionPerformed
 
+    private void btnResetActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnResetActionPerformed
+        // TODO add your handling code here:
+        txtUsername.setText(null);
+        txtPassword.setText(null);
+        cbxPermission.setSelectedIndex(0);
+    }//GEN-LAST:event_btnResetActionPerformed
+
+    private void btnExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnExitActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+    }//GEN-LAST:event_btnExitActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -241,6 +275,7 @@ public class frmLogin extends javax.swing.JDialog {
             public void run() {
                 frmLogin dialog = new frmLogin(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+
                     public void windowClosing(java.awt.event.WindowEvent e) {
                         System.exit(0);
                     }
@@ -250,17 +285,20 @@ public class frmLogin extends javax.swing.JDialog {
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnCancel;
+    private javax.swing.JButton btnExit;
     private javax.swing.JButton btnLogin;
+    private javax.swing.JButton btnReset;
     private javax.swing.JComboBox cbxPermission;
     private javax.swing.JCheckBox chbRepass;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     private javax.swing.JPasswordField jPasswordField1;
-    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator;
     private javax.swing.JLabel lblPassword;
     private javax.swing.JLabel lblPermission;
     private javax.swing.JLabel lblUsername;
+    private javax.swing.JPanel panelInput;
     private javax.swing.JPasswordField txtPassword;
     private javax.swing.JTextField txtUsername;
     // End of variables declaration//GEN-END:variables
