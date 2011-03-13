b525806e63433c0075b4f4d1a2cbe981061333b9
svhaiduong90@gmail.com <svhaiduong90@gmail.com@d9e485e5-d9bd-12cd-a623-42b7acfffde7>
2011-03-13 20:19:13 +0000

    Edit RememberAccount

--- trunk/Certificate Generation System/src/com/hueic/CerGS/dao/RememberAccount.java
+++ trunk/Certificate Generation System/src/com/hueic/CerGS/dao/RememberAccount.java
@@ -7,6 +7,8 @@ package com.hueic.CerGS.dao;
 import com.hueic.CerGS.entity.Account;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.logging.Level;
@@ -18,18 +20,57 @@ import java.util.logging.Logger;
  */
 public class RememberAccount {
 
-    String fileName = "";
+    String fileName = "src/com/hueic/CerGS/temp/UserLogin.properties";
     //thuc hien lay du lieu tu trong file luu tai khoan
 
     public Account getUser() {
         Account acc = null;
-        File file = new  File(fileName);
-        if(file.exists())
-        {
-            acc = new Account();
-            //TODO: lay du lieu tu file remerber password
+        File file = new File(fileName);
+        if (file.exists()) {
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(file);
+                Properties properties = new Properties();
+                properties.load(fis);
+                if (properties.size() != 0) {
+                    acc = new Account();
+                    acc.setUsername(properties.getProperty("username"));
+                    acc.setPassword(properties.getProperty("password"));
+                    //TODO: chua thuc hien ma hoa duoc password trong file log
+                    acc.setPermission(Integer.parseInt(properties.getProperty("permission")));
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(RememberAccount.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                try {
+                    fis.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(RememberAccount.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
         }
-        return null;
+        return acc;
+    }
+
+    public void writeFile(Account acc) {
+        FileOutputStream fos = null;
+        try {
+            Properties properties = new Properties();
+            properties.setProperty("permission", String.valueOf(acc.getPermission()));
+            properties.setProperty("password", acc.getPassword());
+            properties.setProperty("username", acc.getUsername());
+            File file = new File(fileName);
+            fos = new FileOutputStream(file, false);
+            properties.store(fos, null);
+        } catch (IOException ex) {
+            Logger.getLogger(RememberAccount.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException ex) {
+                Logger.getLogger(RememberAccount.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
     //xoa cac du lieu tai khoan da dang nhap vo trong tai khoan
@@ -41,7 +82,6 @@ public class RememberAccount {
             Properties properties = new Properties();
             properties.load(fis);
             properties.clear();
-
         } catch (IOException ex) {
             Logger.getLogger(RememberAccount.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
