ead087ea550a2df1cd1396650a11779996db0dcc
daohuubien <daohuubien@gmail.com>
2011-03-13 05:59:07 +0000

    create class Course, Payment

---
+++ Certificate Generation System/src/com/hueic/CerGS/entity/Course.java
@@ -0,0 +1,77 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.hueic.CerGS.entity;
+
+/**
+ *
+ * @author HuuBien
+ * Version 1.0
+ * lop Course mo ta cac khoa hoc.
+ * ngay 13/3/11
+ */
+public class Course {
+    private String id;
+    private String name;
+    private int status;
+
+    public Course(){
+        setId(null);
+        setStatus(1);
+        setName(null);
+    }
+
+    public Course(String id, String name){
+        setId(id);
+        setName(name);
+        setStatus(1);
+    }
+
+    
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the status
+     */
+    public int getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+
+}
---
+++ Certificate Generation System/src/com/hueic/CerGS/entity/Payment.java
@@ -0,0 +1,110 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.hueic.CerGS.entity;
+
+/**
+ *
+ * @author HuuBien
+ * Version 1.0
+ * Mo ta qua trinh thanh toan tien cua sinh vien
+ * date 13/3/11
+ *
+ */
+public class Payment {
+
+    private int id;
+    private String studentId;
+    private String subjectId;
+    private float money;
+    private String payday;
+
+    public Payment(){
+        setId(0);
+        setStudentId(null);
+        setSubjectId(null);
+        setMoney(0);
+        setPayday(null);
+    }
+
+    public Payment(int id, String studentId, String subjectId, float money, String payday){
+        setId(id);
+        setStudentId(studentId);
+        setSubjectId(subjectId);
+        setMoney(money);
+        setPayday(payday);
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the studentId
+     */
+    public String getStudentId() {
+        return studentId;
+    }
+
+    /**
+     * @param studentId the studentId to set
+     */
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    /**
+     * @return the subjectId
+     */
+    public String getSubjectId() {
+        return subjectId;
+    }
+
+    /**
+     * @param subjectId the subjectId to set
+     */
+    public void setSubjectId(String subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    /**
+     * @return the money
+     */
+    public float getMoney() {
+        return money;
+    }
+
+    /**
+     * @param money the money to set
+     */
+    public void setMoney(float money) {
+        this.money = money;
+    }
+
+    /**
+     * @return the payday
+     */
+    public String getPayday() {
+        return payday;
+    }
+
+    /**
+     * @param payday the payday to set
+     */
+    public void setPayday(String payday) {
+        this.payday = payday;
+    }
+    
+}
