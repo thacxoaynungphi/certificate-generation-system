3d47668629e3bedfb488faffafeae59f09d94f41
nhchung.it <nhchung.it@gmail.com>
2011-03-13 05:08:52 +0000

    Tao class Confirugre, PassEncryption

---
+++ Certificate Generation System/src/com/hueic/CerGS/util/Configure.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.hueic.CerGS.util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author nhchung
+ */
+public class Configure {
+    Connection con = null;
+    public Connection getConnection() {
+
+        //String url = "jdbc:sqlserver://localhost:1433;databaseName=BugReportingSystem";
+        String url = "jdbc:odbc:BugReportingSystem";
+        String user = "sa";
+        String password = "123456";
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            con = java.sql.DriverManager.getConnection(url, user, password);
+        } catch (SQLException ex) {
+            Logger.getLogger(Configure.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Configure.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+    }
+
+    public void closeConnection() {
+        try {
+            if (!con.isClosed()) {
+                con.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Configure.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
