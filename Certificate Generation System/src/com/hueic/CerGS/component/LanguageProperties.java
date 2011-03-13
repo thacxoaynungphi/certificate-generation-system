c213e94f2a9da77aebce0a2d00c1ccc72dd52515
svhaiduong90 <svhaiduong90@gmail.com>
2011-03-13 22:01:10 +0000

    Edit lang

--- Certificate Generation System/src/com/hueic/CerGS/component/LanguageProperties.java
+++ Certificate Generation System/src/com/hueic/CerGS/component/LanguageProperties.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
  */
 public class LanguageProperties {
 
-    public String lang = "vn_VN";
+    public String lang = "fr_FR";
     public String filename = null;
     public LanguageProperties()
     {
