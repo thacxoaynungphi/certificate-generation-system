1dc6b6912778763fa3b2aad07aa2aa7febb221fa
svhaiduong90@gmail.com <svhaiduong90@gmail.com@d9e485e5-d9bd-12cd-a623-42b7acfffde7>
2011-03-21 18:20:59 +0000

    Look and feel

--- trunk/Certificate Generation System/src/com/hueic/CerGS/component/GUIProperties.java
+++ trunk/Certificate Generation System/src/com/hueic/CerGS/component/GUIProperties.java
@@ -14,20 +14,8 @@ public class GUIProperties {
     public static final String PLAF_WINDOWS = "com.sun.java.swing.plaf.windows.WindowsLookAndFeel";
     public static final String PLAF_MOTIF = "com.sun.java.swing.plaf.motif.MotifLookAndFeel";
     public static final String PLAF_MAC = "com.birosoft.liquid.LiquidLookAndFeel";
-    public static final String PLAF_ACRYL = "com.jtattoo.plaf.acryl.AcrylLookAndFeel";
-    public static final String PLAF_AERO = "com.jtattoo.plaf.aero.AeroLookAndFeel";
-    public static final String PLAF_ALUMINIUM = "com.jtattoo.plaf.aluminium.AluminiumLookAndFeel";
-    public static final String PLAF_BERNSTEIN = "com.jtattoo.plaf.bernstein.BernsteinLookAndFeel";
-    public static final String PLAF_FAST = "com.jtattoo.plaf.fast.FastLookAndFeel";
-    public static final String PLAF_GRAPHITE = "com.jtattoo.plaf.graphite.GraphiteLookAndFeel";
-    public static final String PLAF_HIFI = "com.jtattoo.plaf.hifi.HiFiLookAndFeel";
-    public static final String PLAF_LUNA = "com.jtattoo.plaf.luna.LunaLookAndFeel";
-    public static final String PLAF_MCWIN = "com.jtattoo.plaf.mcwin.McWinLookAndFeel";
-    public static final String PLAF_MINT = "com.jtattoo.plaf.mint.MintLookAndFeel";
-    public static final String PLAF_NOIRE = "com.jtattoo.plaf.noire.NoireLookAndFeel";
-    public static final String PLAF_SMART = "com.jtattoo.plaf.smart.SmartLookAndFeel";
-   
-    private String lookAndFeel = PLAF_ACRYL;
+    public static final String PLAF_JGOODIES = "com.jgoodies.looks.plastic.Plastic3DLookAndFeel";
+    private String lookAndFeel = PLAF_JGOODIES;
     private String theme = "Default";
 
     public GUIProperties() {
@@ -67,53 +55,5 @@ public class GUIProperties {
 
     public boolean isMacLook() {
         return lookAndFeel.equals(PLAF_MAC);
-    }
-
-    public boolean isFastLook() {
-        return lookAndFeel.equals(PLAF_FAST);
-    }
-
-    public boolean isGraphiteLook() {
-        return lookAndFeel.equals(PLAF_GRAPHITE);
-    }
-
-    public boolean isSmartLook() {
-        return lookAndFeel.equals(PLAF_SMART);
-    }
-
-    public boolean isAcrylLook() {
-        return lookAndFeel.equals(PLAF_ACRYL);
-    }
-
-    public boolean isAeroLook() {
-        return lookAndFeel.equals(PLAF_AERO);
-    }
-
-    public boolean isBernsteinLook() {
-        return lookAndFeel.equals(PLAF_BERNSTEIN);
-    }
-
-    public boolean isAluminiumLook() {
-        return lookAndFeel.equals(PLAF_ALUMINIUM);
-    }
-
-    public boolean isMcWinLook() {
-        return lookAndFeel.equals(PLAF_MCWIN);
-    }
-
-    public boolean isMintLook() {
-        return lookAndFeel.equals(PLAF_MINT);
-    }
-
-    public boolean isHiFiLook() {
-        return lookAndFeel.equals(PLAF_HIFI);
-    }
-
-    public boolean isNoireLook() {
-        return lookAndFeel.equals(PLAF_NOIRE);
-    }
-
-    public boolean isLunaLook() {
-        return lookAndFeel.equals(PLAF_LUNA);
-    }
+        }
 }
