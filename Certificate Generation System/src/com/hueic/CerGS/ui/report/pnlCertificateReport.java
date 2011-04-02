e1bc7f83bb4e7768ffb982167597ebab81b8e27e
svhaiduong90 <svhaiduong90@gmail.com>
2011-04-02 19:27:41 +0000

    edit report

--- Certificate Generation System/src/com/hueic/CerGS/ui/report/pnlCertificateReport.java
+++ Certificate Generation System/src/com/hueic/CerGS/ui/report/pnlCertificateReport.java
@@ -29,9 +29,8 @@ public class pnlCertificateReport extends javax.swing.JPanel {
     /** Creates new form pnlCertificate */
     public pnlCertificateReport(String studentId) {
         initComponents();
-        setLayout(new BorderLayout());
+       // setLayout(new BorderLayout());
         cerReportManager = new CertificateReportManager(studentId);
-
         try {
             this.add(cerReportManager.getPanelViewer(true));
         } catch (Exception ex) {
@@ -50,16 +49,7 @@ public class pnlCertificateReport extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setLayout(new java.awt.BorderLayout());
     }// </editor-fold>//GEN-END:initComponents
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
