8786f1125cb91bacfcd1776a7834528c5a58e4ac
daohuubien <daohuubien@gmail.com>
2011-04-03 16:07:17 +0000

    fix MarkAll + FeeStudent report

--- Certificate Generation System/src/com/hueic/CerGS/ui/report/pnlFeeReport.java
+++ Certificate Generation System/src/com/hueic/CerGS/ui/report/pnlFeeReport.java
@@ -12,7 +12,6 @@ package com.hueic.CerGS.ui.report;
 
 import com.hueic.CerGS.component.report.FeeReportManager;
 import com.hueic.CerGS.entity.Payment;
-import com.hueic.CerGS.entity.Student;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
@@ -35,6 +34,8 @@ public class pnlFeeReport extends javax.swing.JPanel {
         this.setVisible(true);
     }
 
+    
+
     /** This method is called from within the constructor to
      * initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is
@@ -44,16 +45,7 @@ public class pnlFeeReport extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setLayout(new java.awt.BorderLayout());
     }// </editor-fold>//GEN-END:initComponents
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
